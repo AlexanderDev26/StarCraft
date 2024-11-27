ce0f4298e2cad17f44d3599d6cbc6080fd474409
AlexanderDev26 <alexandercalatayud26@gmail.com>
2024-11-27 07:08:57 -0400

    chore: agregar clase base para el manejo de modelos

---
+++ StarCraft/BaseModel.py
@@ -0,0 +1,11 @@
+# clase padre
+class BaseModel:
+    def __init__(self):
+        def crear(self):
+            pass
+        def Listar(self):
+            pass
+        def Actualizar(self):
+            pass
+        def eliminar(self):
+            pass
